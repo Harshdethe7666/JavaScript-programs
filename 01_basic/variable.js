3205db0f6c6da67e1b6b9f0b6ffa75dfc11607d5
Harsh Dethe <166943904+Harshdethe7666@users.noreply.github.com>
2024-11-06 13:36:08 +0000

    container config done

---
+++ 01_basic/variable.js
@@ -0,0 +1,10 @@
+const accountid=121212
+let accountemail=hd@gmail.com
+var accountpassword=111111
+accountcity='Nagpur'
+let accountState='Maharashtra'
+
+//accountid=1           error occur
+// console.log(accountid);
+
+
