9329833d01d976a6e21a1d66b4433533a27bb125
Harsh Dethe <166943904+Harshdethe7666@users.noreply.github.com>
2024-11-06 14:12:29 +0000

    datatype

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,23 @@
+const accountid=121212
+let accountemail='hd@gmail.com'
+var accountpassword=111111
+accountcity='Nagpur'
+let accountState
+
+//accountid=1           error occur
+// console.log(accountid);
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+accountemail='buddy@gmail.com'
+accountpassword='hdbuddy'
+accountcity='Pune'
+
+console.table([accountid,accountemail,accountpassword,accountcity,accountState]);
+
+
+
