0c50491c4e2c92ae92dff94b5dee13a238875af7
Harsh Dethe <166943904+Harshdethe7666@users.noreply.github.com>
2024-11-07 14:19:12 +0000

    changes

---
+++ 01_basic/05_datatypesummary.js
@@ -0,0 +1,46 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const name='harsh';
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// console.log(typeof score);
+// console.log(typeof scoreValue);
+// console.log(typeof isLoggedIn);
+// console.log(typeof outsideTemp);
+// console.log(typeof userEmail);
+// console.log(typeof id);
+// console.log(typeof name);
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof heros);
+console.log(typeof myFunction);
+console.log(typeof myObj);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
--- 01_basic/text.js
+++ 01_basic/text.js
@@ -1,1 +1,43 @@
 console.log("harsh ")
+
+//*************variable declaration****************
+
+// let data can be changed or override but we cannot redeclared
+let x=0
+// let x=1    we can not do this not redeclared        
+x=1
+console.log(x);
+
+// var data can be changed or override that why it's not recomndeted due to datalose
+// Redeclaring a JavaScript variable with var is allowed anywhere in a program:
+var y=0
+var y=1
+console.log(y);
+
+//Redeclaring a variable inside a block will not redeclare the variable outside the block:
+/* we can do this 
+let x = 10;
+// Here x is 10
+
+{
+let x = 2;
+// Here x is 2
+}
+
+// Here x is 10
+*/
+
+//With let, redeclaring a variable in the same block is NOT allowed:
+/*var x = 2;   // Allowed
+let x = 3;   // Not allowed
+
+{
+let x = 2;   // Allowed
+let x = 3;   // Not allowed
+}
+
+{
+let x = 2;   // Allowed
+var x = 3;   // Not allowed
+}
+*/
